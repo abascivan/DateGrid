88d4eedcbf523dc145d6d5e6e17f8d02dd331729
Ahmet Bascivan <ahmetbascivan@MacBook-Pro-de-Ahmet.local>
2020-11-10 20:51:12 +0100

    c

--- Sources/DateGrid/DateGrid.swift
+++ Sources/DateGrid/DateGrid.swift
@@ -124,7 +124,7 @@ public struct DateGrid<DateView>: View where DateView: View {
                         }
                     }
                 }
-                .tabViewStyle(PageTabViewStyel())
+                .tabViewStyle(PageTabViewStyle())
             }
             .onAppear(){
                 mothsCount = viewModel.months.count
