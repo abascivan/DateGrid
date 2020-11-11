dc8cf9c315d2a3cef42eb7cc34da666eb8b47ec7
Ahmet Bascivan <ahmetbascivan@MacBook-Pro-de-Ahmet.local>
2020-11-11 12:17:31 +0100

    c

--- Sources/DateGrid/DateGrid.swift
+++ Sources/DateGrid/DateGrid.swift
@@ -31,6 +31,8 @@ public struct DateGrid<DateView>: View where DateView: View {
     
     let windowWidth: CGFloat = UIScreen.main.bounds.width
     
+    //PagingScrollView
+    
     public var body: some View {
         
         if #available(iOS 14.0, *) {
@@ -152,29 +154,14 @@ struct PagingScrollView: View {
         self.stackOffset = contentWidth/2 - windowWidth/2
     }
     
-    /// index of current page 0..N-1
     @Binding var activePageIndex : Int
-    
     let windowWidth: CGFloat = UIScreen.main.bounds.width
-    
-    /// total width of conatiner
     private let contentWidth : CGFloat
-    
-    /// since the hstack is centered by default this offset actualy moves it entirely to the left
     private let stackOffset : CGFloat // to fix center alignment
-    
-    /// number of items; I did not come with the soluion of extracting the right count in initializer
     private let itemCount : Int
-    
-    /// some damping factor to reduce liveness
     private let scrollDampingFactor: CGFloat = 0.66
-    
-    /// current offset of all items
     @State var currentScrollOffset: CGFloat = 0
-    
-    /// drag offset during drag gesture
     @State private var dragOffset : CGFloat = 0
-    
     
     func offsetForPageIndex(_ index: Int)->CGFloat {
         let activePageOffset = CGFloat(index)*(windowWidth)
@@ -193,52 +180,48 @@ struct PagingScrollView: View {
         return min(computedIndex, self.itemCount-1)
     }
     
-    /// current scroll offset applied on items
     func computeCurrentScrollOffset()->CGFloat {
         return self.offsetForPageIndex(self.activePageIndex) + self.dragOffset
     }
     
-    /// logical offset startin at 0 for the first item - this makes computing the page index easier
     func logicalScrollOffset(trueOffset: CGFloat)->CGFloat {
         return (trueOffset) * -1.0
     }
     
     
     var body: some View {
-        GeometryReader { outerGeometry in
-            HStack {
-                /// building items into HStack
-                ForEach(0..<self.items.count) { index in
-                    
-                    self.items[index]
-                        .scaledToFill()
-                    
-                }
+        HStack {
+            /// building items into HStack
+            ForEach(0..<self.items.count) { index in
+                
+                self.items[index]
+                    .scaledToFill()
+                
             }
-            .onAppear {
-                self.currentScrollOffset = self.offsetForPageIndex(self.activePageIndex)
-            }
-            .offset(x: self.stackOffset, y: 0)
-            .background(Color.black.opacity(0.00001)) // hack - this allows gesture recognizing even when background is transparent
-            .frame(width: self.contentWidth)
-            .offset(x: self.currentScrollOffset, y: 0)
-            .simultaneousGesture( DragGesture(minimumDistance: 1, coordinateSpace: .local) // can be changed to simultaneous gesture to work with buttons
-                                    .onChanged { value in
-                                        self.dragOffset = value.translation.width
+        }
+        .onAppear {
+            self.currentScrollOffset = self.offsetForPageIndex(self.activePageIndex)
+        }
+        .offset(x: self.stackOffset, y: 0)
+        .background(Color.black.opacity(0.00001)) // hack - this allows gesture recognizing even when background is transparent
+        .frame(width: self.contentWidth)
+        .offset(x: self.currentScrollOffset, y: 0)
+        .simultaneousGesture( DragGesture(minimumDistance: 1, coordinateSpace: .local) // can be changed to simultaneous gesture to work with buttons
+                                .onChanged { value in
+                                    self.dragOffset = value.translation.width
+                                    self.currentScrollOffset = self.computeCurrentScrollOffset()
+                                }
+                                .onEnded { value in
+                                    // compute nearest index
+                                    let velocityDiff = (value.predictedEndTranslation.width - self.dragOffset)*self.scrollDampingFactor
+                                    let newPageIndex = self.indexPageForOffset(self.currentScrollOffset+velocityDiff)
+                                    self.dragOffset = 0
+                                    withAnimation(.interpolatingSpring(mass: 0.1, stiffness: 20, damping: 100, initialVelocity: 0)){
+                                        self.activePageIndex = newPageIndex
                                         self.currentScrollOffset = self.computeCurrentScrollOffset()
                                     }
-                                    .onEnded { value in
-                                        // compute nearest index
-                                        let velocityDiff = (value.predictedEndTranslation.width - self.dragOffset)*self.scrollDampingFactor
-                                        let newPageIndex = self.indexPageForOffset(self.currentScrollOffset+velocityDiff)
-                                        self.dragOffset = 0
-                                        withAnimation(.interpolatingSpring(mass: 0.1, stiffness: 20, damping: 100, initialVelocity: 0)){
-                                            self.activePageIndex = newPageIndex
-                                            self.currentScrollOffset = self.computeCurrentScrollOffset()
-                                        }
-                                    }
-            )
-        }
+                                }
+        )
     }
 }
 
