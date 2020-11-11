c2c98c016b5c7fc67cdf6424df09c6a85994ac07
Ahmet Bascivan <ahmetbascivan@MacBook-Pro-de-Ahmet.local>
2020-11-11 09:51:15 +0100

    c

--- Sources/DateGrid/DateGrid.swift
+++ Sources/DateGrid/DateGrid.swift
@@ -96,7 +96,7 @@ public struct DateGrid<DateView>: View where DateView: View {
                     }
                 }
 //                .frame(width: windowWidth)
-                PagingScrollView(activePageIndex: self.$activePageIndex, itemCount: self.$mothsCount ,pageWidth: windowWidth, tileWidth: windowWidth, tilePadding: 0){
+                PagingScrollView(activePageIndex: self.$activePageIndex, itemCount: self.mothsCount ,pageWidth: windowWidth, tileWidth: windowWidth, tilePadding: 0){
                     ForEach(viewModel.months, id: \.self) { month in
                         VStack {
                             ForEach(0 ..< numberOfDayasInAWeek, id: \.self) { i in
